13dd0cbdca24d4600bd8ab5712e048b2436e4f6a
saraiesco <saraiescobar@google.com>
2023-10-13 14:23:57 -0500

    extensions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { Musician } = require("../models/index")
+const {Band} = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
@@ -12,8 +13,24 @@ app.get('/musicians',async (request,response)=>{
     response.json(musicians)
 })
 
+app.get('/musicians/1',async (request,response)=>{
+    let musicians = await Musician.findAll();
+    response.json(musicians[0])
+})
 
+app.get('/musicians/2',async (request,response)=>{
+    let musicians = await Musician.findAll();
+    response.json(musicians[1])
+})
 
+app.get('/musicians/3',async (request,response)=>{
+    let musicians = await Musician.findAll();
+    response.json(musicians[2])
+})
 
+app.get('/bands',async (request,response)=>{
+    let bands = await Band.findAll();
+    response.json(bands)
+})
 
 module.exports = app;
